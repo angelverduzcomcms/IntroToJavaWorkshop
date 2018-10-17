2b4645ab49a71bc817b3033f75d28c619620526a
angelverduzcomcms <44244265+angelverduzcomcms@users.noreply.github.com>
2018-10-17 13:56:24 -0700

    Add files via upload

--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,23 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot alduin=new Robot();
 		// 5. Set your robot's pen down 
-		
+		alduin.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		alduin.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i = 0; i < 180; i++) {
 			// 7. Change the pen color to random
-		
+		alduin.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			alduin.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		alduin.turn(360/7);
 			// 8. Set the pen width to i
-			
+		alduin.setPenWidth(i);
+		}
+		
+		
 	}
 }
